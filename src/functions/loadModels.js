65cd6ba68cd55b274d695b0a4d6e8aa49fe57a8e
Digiox <xlt84400@gmail.com>
2020-06-10 18:41:11 +0200

    fix(deployFix): fixed the static files access url

--- src/functions/loadModels.js
+++ src/functions/loadModels.js
@@ -2,12 +2,12 @@ import * as face from 'face-api.js'
 
 const loadModels = (callback) => {
     Promise.all([
-        face.loadSsdMobilenetv1Model('/models'),
-        face.loadTinyFaceDetectorModel('/models'),
-        face.loadFaceLandmarkModel('/models'),
-        face.loadFaceLandmarkTinyModel('/models'),
-        face.loadFaceRecognitionModel('/models'),
-        face.loadFaceExpressionModel('/models')
+        face.loadSsdMobilenetv1Model(process.env.PUBLIC_URL+'/models'),
+        face.loadTinyFaceDetectorModel(process.env.PUBLIC_URL+'/models'),
+        face.loadFaceLandmarkModel(process.env.PUBLIC_URL+'/models'),
+        face.loadFaceLandmarkTinyModel(process.env.PUBLIC_URL+'/models'),
+        face.loadFaceRecognitionModel(process.env.PUBLIC_URL+'/models'),
+        face.loadFaceExpressionModel(process.env.PUBLIC_URL+'/models')
     ]).then(async () => {
        callback()
     }).catch(err => console.error(JSON.stringify(err)))
